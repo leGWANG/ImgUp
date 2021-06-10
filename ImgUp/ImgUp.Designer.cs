caa443c9662b0c76d20130df313cc0b284a67f7a
leGWANG <iker0841@naver.com>
2021-06-10 22:47:38 +0900

    MainForm & notifyIcon

--- ImgUp/ImgUp.Designer.cs
+++ ImgUp/ImgUp.Designer.cs
@@ -30,12 +30,81 @@ namespace ImgUp
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImgUp));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ImgUp_pb = new System.Windows.Forms.PictureBox();
+            this.ImgUp_lb = new System.Windows.Forms.Label();
+            this.ImgUp_lnklb = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgUp_pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // ImgUp_pb
+            // 
+            this.ImgUp_pb.BackgroundImage = global::ImgUp.Properties.Resources.ImgUp_Icon;
+            this.ImgUp_pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ImgUp_pb.Location = new System.Drawing.Point(45, 10);
+            this.ImgUp_pb.Name = "ImgUp_pb";
+            this.ImgUp_pb.Size = new System.Drawing.Size(90, 90);
+            this.ImgUp_pb.TabIndex = 0;
+            this.ImgUp_pb.TabStop = false;
+            // 
+            // ImgUp_lb
+            // 
+            this.ImgUp_lb.AutoSize = true;
+            this.ImgUp_lb.Font = new System.Drawing.Font("돋움", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ImgUp_lb.Location = new System.Drawing.Point(42, 110);
+            this.ImgUp_lb.Name = "ImgUp_lb";
+            this.ImgUp_lb.Size = new System.Drawing.Size(95, 15);
+            this.ImgUp_lb.TabIndex = 1;
+            this.ImgUp_lb.Text = "ImgUp (v1.0)";
+            this.ImgUp_lb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ImgUp_lnklb
+            // 
+            this.ImgUp_lnklb.AutoSize = true;
+            this.ImgUp_lnklb.Font = new System.Drawing.Font("돋움", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ImgUp_lnklb.Location = new System.Drawing.Point(70, 135);
+            this.ImgUp_lnklb.Name = "ImgUp_lnklb";
+            this.ImgUp_lnklb.Size = new System.Drawing.Size(43, 13);
+            this.ImgUp_lnklb.TabIndex = 2;
+            this.ImgUp_lnklb.TabStop = true;
+            this.ImgUp_lnklb.Text = "Github";
+            this.ImgUp_lnklb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ImgUp_lnklb.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ImgUp_lnklb_LinkClicked);
+            // 
+            // ImgUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.Controls.Add(this.ImgUp_lnklb);
+            this.Controls.Add(this.ImgUp_lb);
+            this.Controls.Add(this.ImgUp_pb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ImgUp";
+            this.Load += new System.EventHandler(this.ImgUp_Load);
+            this.Resize += new System.EventHandler(this.ImgUp_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgUp_pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.PictureBox ImgUp_pb;
+        private System.Windows.Forms.Label ImgUp_lb;
+        private System.Windows.Forms.LinkLabel ImgUp_lnklb;
     }
 }
 
--- ImgUp/ImgUp.cs
+++ ImgUp/ImgUp.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace ImgUp
 {
@@ -15,6 +17,38 @@ namespace ImgUp
         public ImgUp()
         {
             InitializeComponent();
+        }
+        private void ImgUp_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ImgUp_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void ImgUp_Show()
+        {
+            if(this.Visible == false && notifyIcon.Visible == true)
+            {
+                this.Visible = true;
+                notifyIcon.Visible = false;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            ImgUp_Show();
+        }
+
+        private void ImgUp_lnklb_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/leGWANG/ImgUp");
         }
     }
 }
